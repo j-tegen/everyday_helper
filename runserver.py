720d2af06697aa71caab3153fbda09ecff9e7dc2
Kahrstrom <jonatan.kahrstrom@gmail.com>
2017-10-11 21:39:22 +0200

    Test port

--- runserver.py
+++ runserver.py
@@ -2,5 +2,6 @@ from project.server import app, config
 import os
 
 if __name__ == "__main__":
-	port = int(os.environ.get('‌​PORT', 5000))
+	port = int(os.environ.get('PORT', 5000))
+	print(port)
 	app.run(host=config.host_ip, port=port,debug=False)
